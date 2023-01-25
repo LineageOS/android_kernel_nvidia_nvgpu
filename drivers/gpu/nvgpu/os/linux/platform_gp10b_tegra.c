391bc08dfa52e9e4beebe9d2ac6d1283fda61193
Aaron Kling <webgeek1234@gmail.com>
2023-01-25 13:16:07 -0600

    fixup! gpu: nvgpu: register pm_qos min & max frequency notifiers
    Copy support to gp10b
    
    Change-Id: I60b50827d38f2e5e4956d01c143c3c20fa766290

--- drivers/gpu/nvgpu/os/linux/platform_gp10b_tegra.c
+++ drivers/gpu/nvgpu/os/linux/platform_gp10b_tegra.c
@@ -576,7 +576,12 @@ struct gk20a_platform gp10b_tegra_platform = {
 	.postscale = gp10b_tegra_postscale,
 	.devfreq_governor = "nvhost_podgov",
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 10, 0)
+	.qos_min_notify = gk20a_scale_qos_min_notify,
+	.qos_max_notify = gk20a_scale_qos_max_notify,
+#else
 	.qos_notify = gk20a_scale_qos_notify,
+#endif /* LINUX_VERSION_CODE >= KERNEL_VERSION(5, 10, 0) */
 
 	.reset_assert = gp10b_tegra_reset_assert,
 	.reset_deassert = gp10b_tegra_reset_deassert,
